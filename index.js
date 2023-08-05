e5c56420e56be2fe5c980121595c6eb8faea3252
vbcode19 <batravarun698@gmail.com>
2023-08-05 10:02:48 +0530

    Movement of  player

--- index.js
+++ index.js
@@ -9,8 +9,9 @@ class Player {
    constructor() {
       this.velocity = {
          x: 0,
-         y: 0,
+         y: 0
       }
+      this.rotation = 0;
       const image = new Image()
       image.src = './images/spaceship.png'
 
@@ -21,7 +22,7 @@ class Player {
          this.height = image.height * scale
          this.position = {
             x: canvas.width / 2 - this.width / 2,
-            y: canvas.height - this.height
+            y: canvas.height - this.height - 20
          }
       }
    }
@@ -29,21 +30,105 @@ class Player {
    draw() {
       // c.fillStyle = 'red'
       // c.fillRect(this.position.x, this.position.y, this.width, this.height)
-      if (this.image)
-         c.drawImage(this.image, this.position.x, this.position.y, this.width, this.height)
 
+      // save the current state of the player including rotations
+      c.save()
+      c.translate(player.position.x + player.width / 2, player.position.y + player.height / 2)
+
+      c.rotate(this.rotation)
+
+      c.translate(
+         -player.position.x - player.width / 2,
+         -player.position.y - player.height / 2
+      )
+
+      c.drawImage(this.image,
+         this.position.x,
+         this.position.y,
+         this.width,
+         this.height
+      )
+
+      // this is used to the restore the drawing context to the previously 
+      c.restore()
+
+   }
+   update() {
+      if (this.image) {
+         this.draw()
+         this.position.x += this.velocity.x
+      }
    }
 }
 
 const player = new Player()
-player.draw()
+// player.draw()
+const keys = {
+   a: {
+      pressed: false,
+   },
+   d: {
+      pressed: false,
+   },
+   space: {
+      pressed: false,
+   },
+}
 
 function animate() {
    requestAnimationFrame(animate)
    c.fillStyle = 'black'
    c.fillRect(0, 0, canvas.width, canvas.height)
-   player.draw()
+   player.update()
+
+   if (keys.a.pressed && player.position.x >= 0) {
+      player.velocity.x = -5
+      player.rotation = -0.15
+   }
+   else if (keys.d.pressed && player.position.x + player.width <= canvas.width) {
+      player.velocity.x = 5
+      player.rotation = 0.15
+   }
+   else {
+      player.velocity.x = 0;
+      player.rotation = 0;
+   }
 }
 
 animate()
+
+addEventListener('keydown', ({ key }) => {
+   switch (key) {
+      case 'a':
+         console.log('left')
+         keys.a.pressed = true
+         break;
+
+      case 'd':
+         console.log('right')
+         keys.d.pressed = true
+         break;
+
+      case ' ':
+         console.log('space')
+         break;
+   }
+})
+addEventListener('keyup', ({ key }) => {
+   switch (key) {
+      case 'a':
+         console.log('left')
+         keys.a.pressed = false
+         break;
+
+      case 'd':
+         console.log('right')
+         keys.d.pressed = false
+         break;
+
+      case ' ':
+         console.log('space')
+         break;
+   }
+})
 
